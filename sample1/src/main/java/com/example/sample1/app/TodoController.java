b1fea7912dcb0efc086b0fb246ac63bd7789d114
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-08-12 08:26:28 +0900

    add:仮html作成

---
+++ sample1/src/main/java/com/example/sample1/app/TodoController.java
@@ -0,0 +1,18 @@
+package com.example.sample1.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+	public TodoController() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "todo/index";
+	}
+}
