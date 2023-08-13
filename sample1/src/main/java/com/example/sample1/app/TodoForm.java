97d94d83eaf0677977af651aaba052821d807a78
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-08-13 18:28:38 +0900

    update:titleを20文字までに変更

--- sample1/src/main/java/com/example/sample1/app/TodoForm.java
+++ sample1/src/main/java/com/example/sample1/app/TodoForm.java
@@ -2,18 +2,18 @@ package com.example.sample1.app;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TodoForm {
 	public int status;
 	@NotBlank
-	@Size(max = 50)
+	@Length(max = 20)
 	public String title;
 	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
